386bc78da42167312880824391ad463017912682
liwenyou <lwy20068957@126.com>
2024-04-27 21:10:33 +0800

    Initial Commit

---
+++ FirstWorkJob/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstWorkJob
+//
+//  Created by 李文友 on 2024/4/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
